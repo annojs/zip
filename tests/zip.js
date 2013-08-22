887e6bbc9eef54dc2058fb2c6e0d0aebd570c996
Juho Vepsalainen <bebraw@gmail.com>
2013-08-22 19:20:24 +0300

    Implement object portion

--- tests/zip.js
+++ tests/zip.js
@@ -1,24 +1,28 @@
 var generate = require('annogenerate');
 var fuzz = require('annofuzz')(generate);
 var is = require('annois');
-var equals = require('funkit').ops.equals;
+var funkit = require('funkit');
+var equals = funkit.ops.equals;
+var keys = funkit.object.keys;
+var values = funkit.object.values;
 
 var zip = require('../');
 
 
 fuzz._amount = 100;
 fuzz(zip, function(op, a, b) {
-    var res = op(a, b);
-    var matches;
+    var res, matches;
 
-    if(b) {
-        matches = res.filter(function(v, i) {
-            return equals(v[0], a[i]) && equals(v[1], b[i]);
-        });
-
-        return matches.length == res.length;
+    if(!is.defined(b)) {
+        a = keys(a);
+        b = values(a);
     }
-    // TODO: test object case
 
-    return true;
+    res = op(a, b);
+
+    matches = res.filter(function(v, i) {
+        return equals(v[0], a[i]) && equals(v[1], b[i]);
+    });
+
+    return matches.length == res.length;
 });
