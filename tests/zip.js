d280e388bc66f7331a791a152500868a98c95abf
Juho Vepsalainen <bebraw@gmail.com>
2013-08-22 19:11:59 +0300

    Implement array portion

--- tests/zip.js
+++ tests/zip.js
@@ -1,8 +1,24 @@
-var fuzz = require('annofuzz');
+var generate = require('annogenerate');
+var fuzz = require('annofuzz')(generate);
 var is = require('annois');
+var equals = require('funkit').ops.equals;
 
 var zip = require('../');
 
 
 fuzz._amount = 100;
-// fuzz(zip, function(op, a, b)) // TODO
+fuzz(zip, function(op, a, b) {
+    var res = op(a, b);
+    var matches;
+
+    if(b) {
+        matches = res.filter(function(v, i) {
+            return equals(v[0], a[i]) && equals(v[1], b[i]);
+        });
+
+        return matches.length == res.length;
+    }
+    // TODO: test object case
+
+    return true;
+});
